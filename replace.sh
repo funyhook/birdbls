4261365e06199fdcf98523b0386f9aed3bbc6580
funyhook <funyhook@163.com>
2021-12-17 11:48:20 +0800

    清理优化

--- replace.sh
+++ replace.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
-cd /ql/scripts/ 
+cd ../scripts/ 
 
 arr=("zero205" "Aaron-lv" "lsh26" "transfer" "fastgit" "smiek2121" "lukelucky6" "fatelight")
 
-sed -i  's/zero205/test/' `grep -rl zero205 ./`
-sed -i  's/Aaron-l/test/' `grep -rl Aaron-l ./`
-sed -i  's/lsh26/test/' `grep -rl lsh26 ./`
-sed -i  's/transfer/test/' `grep -rl transfer ./`
-sed -i  's/fastgit/test/' `grep -rl fastgit ./`
-sed -i  's/smiek2121/test/' `grep -rl smiek2121 ./`
-sed -i  's/jdsharecode/test/g' `grep -rl jdsharecode  ./`
-sed -i  's/fatelight/test/g' `grep -rl fatelight  ./`
-sed -i  's/lukelucky6/test/g' `grep -rl lukelucky6  ./`
+#echo ${array[0]}
+for((i=0;i<${#arr[@]};i++));
+do
+    echo "正在清理${arr[$i]}。。。。。。"
+    sleep 1
+    sed -i  's/'${arr[$i]}'/test/' `grep -rl ${arr[$i]} ./`>/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "已清理过，跳过"
+    else
+        echo "${arr[$i]}库清理成功！"
+    fi 
+done
 
 #clean inviteCodes
+echo "正在清理偷助力1。。。。。。"
 sed -i "s/$.newShareCodes \= inviteCodes/\/\//g" `grep "$.newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
- if [ $? -ne 0 ]; then
-        echo "已清理过，跳过"
-    else
-        echo "inviteCodes库清理成功！"
-    fi  
+if [ $? -ne 0 ]; then
+    echo "【偷助力1】已清理过，跳过"
+else
+    echo "【偷助力1】助力清理成功！"
+fi  
 
+echo "正在清理偷助力2。。。。。。"
 sed -i "s/newShareCodes \= inviteCodes/\/\//g" `grep "newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
- if [ $? -ne 0 ]; then
-        echo "已清理过，跳过"
-    else
-        echo "inviteCodes库清理成功！"
-    fi  
+if [ $? -ne 0 ]; then
+    echo "【偷助力2】已清理过，跳过"
+else
+    echo "【偷助力2】助力清理成功！"
+fi  
+    
+echo "正在清理偷邀请1。。。。。。"
+sed -i "s/$.newShareCodes \= shareCodes/\/\//g" `grep "$.newShareCodes \= shareCodes" -rl  ./*`>/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "【偷邀请1】已清理过，跳过"
+else
+    echo "【偷邀请1】助力清理成功！"
+fi  
+    
+echo "正在清理偷邀请2。。。。。。"
+sed -i "s/newShareCodes \= shareCodes/\/\//g" `grep "newShareCodes \= shareCodes" -rl  ./*`>/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "【偷邀请2】已清理过，跳过"
+else
+    echo "【偷邀请2】助力清理成功！"
+fi  
+    
+    
+    
