97b6ce94b6ff88c43b3f9542dd0cf37be5ffeff6
funyhook <funyhook@163.com>
2022-02-17 16:47:05 +0800

    Create jd_decode.js

---
+++ sillyGirl/replies/jd_decode.js
@@ -0,0 +1,21 @@
+//[rule:code ?]//京东口令解析
+//作者：微信公众号【玩机匠】！
+var code = param(1);
+sendText("正在解析口令，请稍等片刻......")
+var _data = {"code": code}
+request({
+    url: 'https://api.jds.codes/jd/jCommand',
+    method: 'POST',
+    dataType:'json',
+    headers: {
+        "content-type": "application/json",
+    },
+    body: _data
+},function(err, resp, data) {
+    if (!err && resp.statusCode == 200) {
+    	if(data){
+    		sendText(data.data.jumpUrl)}
+    }else{
+    		sendText("网络请求失败："+data.msg)
+    	}
+});
