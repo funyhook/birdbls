5e93f45c879a192167e5883f58946277a47dba1a
funyhook <funyhook@163.com>
2022-02-19 14:19:10 +0800

    Update clear.sh
    清理开卡助力

--- clean/clear.sh
+++ clean/clear.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#by：微信公众号：玩机匠
 cd ../scripts/ 
 
-arr=("zero205" "Aaron-lv" "lsh26" "transfer" "shufflewzc" "fastgit" "smiek2121" "smeik2221" "lukelucky6" "fatelight" "Annyoo2021" "KingRan521" "smiek")
+arr=("zero205" "Aaron-lv" "lsh26" "transfer" "fastgit" "shufflewzc" "smiek2121" "smiek2221" "lukelucky6" "fatelight" "Annyoo2021" "KingRan521" "smiek")
+
+
 
 #echo ${array[0]}
 for((i=0;i<${#arr[@]};i++));
 do
     echo "正在清理${arr[$i]}。。。。。。"
-    sleep 1
     sed -i  's/'${arr[$i]}'/test/' `grep -rl ${arr[$i]} ./`>/dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo "已清理过，跳过"
@@ -19,6 +19,7 @@ done
 
 #clean inviteCodes
 echo "正在清理偷助力1。。。。。。"
+sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/$.newShareCodes \= inviteCodes/\/\//g" `grep "$.newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "【偷助力1】已清理过，跳过"
@@ -27,6 +28,7 @@ else
 fi  
 
 echo "正在清理偷助力2。。。。。。"
+sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/newShareCodes \= inviteCodes/\/\//g" `grep "newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "【偷助力2】已清理过，跳过"
@@ -35,6 +37,7 @@ else
 fi  
     
 echo "正在清理偷邀请1。。。。。。"
+sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/$.newShareCodes \= shareCodes/\/\//g" `grep "$.newShareCodes \= shareCodes" -rl  ./*`>/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "【偷邀请1】已清理过，跳过"
@@ -43,12 +46,13 @@ else
 fi  
     
 echo "正在清理偷邀请2。。。。。。"
+sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/newShareCodes \= shareCodes/\/\//g" `grep "newShareCodes \= shareCodes" -rl  ./*`>/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "【偷邀请2】已清理过，跳过"
 else
     echo "【偷邀请2】助力清理成功！"
 fi  
-    
+
     
     
