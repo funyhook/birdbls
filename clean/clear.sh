fa7bd3cd16ca692889212aab5e74b9a3a408e940
635723451@qq.com <xyz13673901243>
2022-02-27 22:53:49 +0800

    又发现一个偷助力库！！！！！！

--- clean/clear.sh
+++ clean/clear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../scripts/ 
 
-arr=("zero205" "Aaron-lv" "lsh26" "transfer" "fastgit" "shufflewzc" "smiek2121" "smiek2221" "lukelucky6" "fatelight" "Annyoo2021" "KingRan521" "smiek")
+arr=("JD-Scripts" "zero205" "Aaron-lv" "lsh26" "transfer" "fastgit" "shufflewzc" "smiek2121" "smiek2221" "lukelucky6" "fatelight" "Annyoo2021" "KingRan521" "smiek")
 echo "#by：微信公众号：玩机匠"
 
 
@@ -19,6 +19,7 @@ done
 
 #clean inviteCodes
 echo "正在清理偷助力1。。。。。。"
+sed -i "s/0ca1c88a3437428c9f0c2376af590c7f/ba7064c41dba48c5a8078cef2da0f6e2/g" `grep "0ca1c88a3437428c9f0c2376af590c7f" -rl  ./*`>/dev/null 2>&1
 sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/$.newShareCodes \= inviteCodes/\/\//g" `grep "$.newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
 if [ $? -ne 0 ]; then
