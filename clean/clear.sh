dfc9398e1388911ce3905aba82c292d3e4d2a8ab
funyhook <funyhook@163.com>
2022-03-01 11:08:15 +0800

    恶心的一批

--- clean/clear.sh
+++ clean/clear.sh
@@ -19,6 +19,7 @@ done
 
 #clean inviteCodes
 echo "正在清理偷助力1。。。。。。"
+sed -i "s/deb51eda516f4ad481fccb34b4a65203/564e43ecaa414556a7b3d42aa9a7038d/g" `grep "564e43ecaa414556a7b3d42aa9a7038d" -rl  ./*`>/dev/null 2>&1
 sed -i "s/0ca1c88a3437428c9f0c2376af590c7f/ba7064c41dba48c5a8078cef2da0f6e2/g" `grep "0ca1c88a3437428c9f0c2376af590c7f" -rl  ./*`>/dev/null 2>&1
 sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/$.newShareCodes \= inviteCodes/\/\//g" `grep "$.newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
@@ -29,6 +30,7 @@ else
 fi  
 
 echo "正在清理偷助力2。。。。。。"
+sed -i "s/deb51eda516f4ad481fccb34b4a65203/564e43ecaa414556a7b3d42aa9a7038d/g" `grep "564e43ecaa414556a7b3d42aa9a7038d" -rl  ./*`>/dev/null 2>&1
 sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/newShareCodes \= inviteCodes/\/\//g" `grep "newShareCodes \= inviteCodes" -rl  ./*` >/dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -38,6 +40,7 @@ else
 fi  
     
 echo "正在清理偷邀请1。。。。。。"
+sed -i "s/deb51eda516f4ad481fccb34b4a65203/564e43ecaa414556a7b3d42aa9a7038d/g" `grep "564e43ecaa414556a7b3d42aa9a7038d" -rl  ./*`>/dev/null 2>&1
 sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/$.newShareCodes \= shareCodes/\/\//g" `grep "$.newShareCodes \= shareCodes" -rl  ./*`>/dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -47,6 +50,7 @@ else
 fi  
     
 echo "正在清理偷邀请2。。。。。。"
+sed -i "s/deb51eda516f4ad481fccb34b4a65203/564e43ecaa414556a7b3d42aa9a7038d/g" `grep "564e43ecaa414556a7b3d42aa9a7038d" -rl  ./*`>/dev/null 2>&1
 sed -i "s/k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf/pxQwblvEb6l5N7qNrN9VWF4tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w==/g" `grep "k1Nobb+P0er+C2sysxnx\/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ\/e9r\/uf" -rl  ./*`>/dev/null 2>&1
 sed -i "s/newShareCodes \= shareCodes/\/\//g" `grep "newShareCodes \= shareCodes" -rl  ./*`>/dev/null 2>&1
 if [ $? -ne 0 ]; then
