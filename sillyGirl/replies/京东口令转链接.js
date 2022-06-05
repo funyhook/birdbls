4ebd02f687b47b87a76508cc0ef393202193a292
funyhook <funyhook@163.com>
2022-06-05 09:48:01 +0800

    更新口令解析接口

--- sillyGirl/replies/京东口令转链接.js
+++ sillyGirl/replies/京东口令转链接.js
@@ -5,7 +5,7 @@ var code = param(1);
 sendText("正在解析口令，请稍等片刻......")
 var _data = {"code": code}
 request({
-    url: 'https://api.jds.codes/jd/jCommand',
+    url: 'https://api.windfgg.cf/jd/code',
     method: 'POST',
     dataType:'json',
     headers: {
@@ -17,6 +17,6 @@ request({
     	if(data){
     		sendText(data.data.jumpUrl)}
     }else{
-    		sendText("【玩机匠】提醒：网络请求失败："+data.msg)
+    		sendText("【玩机匠】提醒：网络请求失败："+JSON.stringify(data))
     	}
 });
