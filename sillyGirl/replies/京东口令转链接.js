9f4586e8c08461bf7a96d2774006eff3c0796f69
funyhook <shaoyang_lian@uyoung.com.cn>
2023-07-21 10:22:03 +0800

    改为wind接口

--- sillyGirl/replies/京东口令转链接.js
+++ sillyGirl/replies/京东口令转链接.js
@@ -1,7 +1,9 @@
 //功能：京东口令解析
 //作者：微信公众号【玩机匠】！
 //[rule:code ?] code ￥FIDLKSN&￥
-var code = param(1);
+var code = param(1)
+sendText("请输入接口token")
+let token = input(6000)
 sendText("正在解析口令，请稍等片刻......")
 var _data = {"code": code}
 request({
@@ -11,7 +13,7 @@ request({
     headers: {
         "content-type": "application/json",
     },
-    Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU0NjA3MTI5OCwiaWF0IjoxNjUzMzk3MzQwLCJleHAiOjE2ODQ5MzMzNDB9.Qzn2r8_YYR6ZjUpttA-oy4KmHxgSrrEiP8lRusW8hY4",
+    Authorization:"Bearer "+token,
     body: _data
 },function(err, resp, data) {
     if(err){
@@ -20,7 +22,7 @@ request({
     if (resp.statusCode == 200 && data) {
         sendText(data.data.jumpUrl)
     }else if (resp.statusCode==401) {
-        sendText("【玩机匠】提醒：暂无接口请求权限："+resp.statusCode)
+        sendText("【玩机匠】提醒：token无效 搞个毛？："+resp.statusCode)
     }else{
         sendText("【玩机匠】提醒：网络异常，稍后重试！：")
     }
